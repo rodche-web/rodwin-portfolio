dfaa904f6eb6574b9b6c49a2953c8aadb71d0da7
rodwin espiritu <rodwin.espiritu@maroonstudios.com>
2023-09-21 08:46:34 +0800

    feat: about me updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
       <div className="max-w-screen-xl mx-auto flex flex-col space-x-12 justify-center sm:flex-row gap-12 mt-12 scroll-mt-40">
         <article className="sm:w-1/2">
           <p className="max-w-lg text-xl mt-4 text-center sm:text-left text-slate-900 dark:text-white">
-            I'm Rodwin Chester Espiritu, a self-taught front-end web developer specializing in HTML, CSS, JavaScript, and the frameworks React.js and Vue.js. With a passion for crafting visually appealing and user-friendly websites, I bring ideas to life through clean and interactive code. My expertise in various Javascript frameworks allows me to build dynamic single-page applications and intuitive user interfaces. I thrive in collaborative environments, bridging the gap between design and functionality.
+            I'm Rodwin Chester Espiritu, a self-taught software engineer specializing in HTML, CSS, JavaScript, and the frameworks React.js and Vue.js. With a passion for crafting visually appealing and user-friendly websites, I bring ideas to life through clean and interactive code. My expertise in various Javascript frameworks allows me to build dynamic single-page applications and intuitive user interfaces. I thrive in collaborative environments, bridging the gap between design and functionality.
           </p>
           <p className="max-w-lg text-xl mt-4 text-center sm:text-left text-slate-900 dark:text-white">
             Skills: HTML, CSS, Javascript, React.js, Vue.js
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
       <section id="hero" className="max-w-7xl mx-auto flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap mb-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
             <article className="sm:w-1/2">
                 <h2 className="max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left text-white">
-                  Hi! I'm Rodwin
+                  Hi! I'm Rodwin.
                 </h2>
                 <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-white">
-                    I am a front-end developer with extensive experience of over 2 years.
+                    I am a software engineer.
                 </p>
-                <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-white">
+                <p className="max-w-md text-2xl mt-4 mb-7 text-center sm:text-left text-white">
                     My expertise is to create web sites and web apps.
                 </p>
             </article>
